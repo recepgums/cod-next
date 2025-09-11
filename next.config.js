b153a9afa07051cc9533b4a5a34c1620c6dc827d
Recep G <recep.gumus@dokuclinic.com>
2025-09-11 10:32:59 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['cod.laravel','localhost','127.0.0.1','main.trendygoods.com.tr', 'demo.trendygoods.com.tr','10.171.250.230:8000', '10.0.19.82', 'https://main.vakuumcar.com.tr','192.168.137.1:8000'],
+    domains: ['cod.laravel','localhost','127.0.0.1','main.trendygoods.com.tr', 'demo.trendygoods.com.tr','10.171.250.230:8000', '10.0.19.82', 'https://main.vakuumcar.com.tr','192.168.137.1:8000','https://codpanel.com.tr'],
   },
 }; 
