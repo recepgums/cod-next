805d2c85f67b0a4b7edd5ad33b87ebbebc21d40e
Recep G <recep.gumus@dokuclinic.com>
2025-09-11 10:43:35 +0300

    replace next config ts to nextconfigjs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'cod.laravel',
+      'localhost',
+      '127.0.0.1',
+      'main.trendygoods.com.tr',
+      'demo.trendygoods.com.tr',
+      '10.171.250.230:8000',
+      '10.0.19.82',
+      'main.vakuumcar.com.tr',
+      '192.168.137.1:8000',
+      'codpanel.com.tr'
+    ],
+  },
 };
 
 export default nextConfig;
