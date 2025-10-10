583dc90a5b9f0fb08b4063f24b4347932d10ca7b
Bedirhan Damar <bedirhandamarr@gmail.com>
2025-10-10 19:50:04 +0300

    button bug fix

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -50,7 +50,15 @@ export default async function Header() {
               priority
             />
           </Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button 
+            className="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav" 
+            aria-controls="navbarNav" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
