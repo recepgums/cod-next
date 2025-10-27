f2ee2518c9194758b8be5bbb0ce7a7d6ef50bbd9
Bedirhan Damar <bedirhandamarr@gmail.com>
2025-10-27 15:20:03 +0300

    nova

--- src/app/product/[slug]/NovaTemplate.tsx
+++ src/app/product/[slug]/NovaTemplate.tsx
@@ -390,37 +390,6 @@ export default function NovaTemplate({ product }: { product: Product }) {
                             <span>Sepete ekle</span>
                         </button>
                     </div>
-
-                    {/* Product Content (HTML) */}
-                    {memoizedProductContent && (
-                        <div className="product-content mb-3" dangerouslySetInnerHTML={memoizedProductContent} />
-                    )}
-
-                    <div className="campaign-container">
-                        {(parsedSettings?.cash_payment_cost == null || parsedSettings?.cash_payment_cost == 0) && (
-                            <div className="campaign-item">
-                                <span className="campaign-icon">
-                                    <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor" data-id="508817602426306920">
-                                        <path fill="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M255.14,115.54l-14-35A19.89,19.89,0,0,0,222.58,68H196V64a12,12,0,0,0-12-12H32A20,20,0,0,0,12,72V184a20,20,0,0,0,20,20H46.06a36,36,0,0,0,67.88,0h44.12a36,36,0,0,0,67.88,0H236a20,20,0,0,0,20-20V120A21.7,21.7,0,0,0,255.14,115.54ZM196,92h23.88l6.4,16H196ZM80,204a12,12,0,1,1,12-12A12,12,0,0,1,80,204Zm92-41.92A36.32,36.32,0,0,0,158.06,180H113.94a36,36,0,0,0-67.88,0H36V140H172ZM172,116H36V76H172Zm20,88a12,12,0,1,1,12-12A12,12,0,0,1,192,204Zm40-24h-6.06A36.09,36.09,0,0,0,196,156.23V132h36Z"></path></svg>
-                                </span>
-                                <span className="campaign-text">
-                                    Ücretsiz Kargo
-                                </span>
-
-                            </div>
-                        )}
-
-
-                        <div className="campaign-item">
-                            <span className="campaign-icon">
-                                <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor" data-id="508817577244295528">
-                                    <path fill="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M232.76,137.88A28.39,28.39,0,0,0,208.13,133L172,141.26c0-.42,0-.84,0-1.26a32,32,0,0,0-32-32H89.94a35.76,35.76,0,0,0-25.45,10.54L43,140H20A20,20,0,0,0,0,160v40a20,20,0,0,0,20,20H120a11.89,11.89,0,0,0,2.91-.36l64-16a11.4,11.4,0,0,0,1.79-.6l38.82-16.54c.23-.09.45-.19.67-.3a28.61,28.61,0,0,0,4.57-48.32ZM36,196H24V164H36Zm181.68-31.39-37.51,16L118.52,196H60V157l21.46-21.46A11.93,11.93,0,0,1,89.94,132H140a8,8,0,0,1,0,16H112a12,12,0,0,0,0,24h32a12.19,12.19,0,0,0,2.69-.3l67-15.41.47-.12a4.61,4.61,0,0,1,5.82,4.44A4.58,4.58,0,0,1,217.68,164.61ZM164,100a40.36,40.36,0,0,0,5.18-.34,40,40,0,1,0,29.67-59.32A40,40,0,1,0,164,100Zm40-36a16,16,0,1,1-16,16A16,16,0,0,1,204,64ZM164,44a16,16,0,0,1,12.94,6.58A39.9,39.9,0,0,0,164.2,76H164a16,16,0,0,1,0-32Z"></path></svg>
-                            </span>
-                            <span className="campaign-text">
-                                Ücretsiz İade
-                            </span>
-                        </div>
-                    </div>
                     <div className="faq-container">
                         <div className="faq-list">
                             {parsedFaq.map((item, index) => (
@@ -460,6 +429,38 @@ export default function NovaTemplate({ product }: { product: Product }) {
                             ))}
                         </div>
                     </div>
+
+                    {/* Product Content (HTML) */}
+                    {memoizedProductContent && (
+                        <div className="product-content mb-3" dangerouslySetInnerHTML={memoizedProductContent} />
+                    )}
+
+                    <div className="campaign-container">
+                        {(parsedSettings?.cash_payment_cost == null || parsedSettings?.cash_payment_cost == 0) && (
+                            <div className="campaign-item">
+                                <span className="campaign-icon">
+                                    <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor" data-id="508817602426306920">
+                                        <path fill="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M255.14,115.54l-14-35A19.89,19.89,0,0,0,222.58,68H196V64a12,12,0,0,0-12-12H32A20,20,0,0,0,12,72V184a20,20,0,0,0,20,20H46.06a36,36,0,0,0,67.88,0h44.12a36,36,0,0,0,67.88,0H236a20,20,0,0,0,20-20V120A21.7,21.7,0,0,0,255.14,115.54ZM196,92h23.88l6.4,16H196ZM80,204a12,12,0,1,1,12-12A12,12,0,0,1,80,204Zm92-41.92A36.32,36.32,0,0,0,158.06,180H113.94a36,36,0,0,0-67.88,0H36V140H172ZM172,116H36V76H172Zm20,88a12,12,0,1,1,12-12A12,12,0,0,1,192,204Zm40-24h-6.06A36.09,36.09,0,0,0,196,156.23V132h36Z"></path></svg>
+                                </span>
+                                <span className="campaign-text">
+                                    Ücretsiz Kargo
+                                </span>
+
+                            </div>
+                        )}
+
+
+                        <div className="campaign-item">
+                            <span className="campaign-icon">
+                                <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor" data-id="508817577244295528">
+                                    <path fill="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M232.76,137.88A28.39,28.39,0,0,0,208.13,133L172,141.26c0-.42,0-.84,0-1.26a32,32,0,0,0-32-32H89.94a35.76,35.76,0,0,0-25.45,10.54L43,140H20A20,20,0,0,0,0,160v40a20,20,0,0,0,20,20H120a11.89,11.89,0,0,0,2.91-.36l64-16a11.4,11.4,0,0,0,1.79-.6l38.82-16.54c.23-.09.45-.19.67-.3a28.61,28.61,0,0,0,4.57-48.32ZM36,196H24V164H36Zm181.68-31.39-37.51,16L118.52,196H60V157l21.46-21.46A11.93,11.93,0,0,1,89.94,132H140a8,8,0,0,1,0,16H112a12,12,0,0,0,0,24h32a12.19,12.19,0,0,0,2.69-.3l67-15.41.47-.12a4.61,4.61,0,0,1,5.82,4.44A4.58,4.58,0,0,1,217.68,164.61ZM164,100a40.36,40.36,0,0,0,5.18-.34,40,40,0,1,0,29.67-59.32A40,40,0,1,0,164,100Zm40-36a16,16,0,1,1-16,16A16,16,0,0,1,204,64ZM164,44a16,16,0,0,1,12.94,6.58A39.9,39.9,0,0,0,164.2,76H164a16,16,0,0,1,0-32Z"></path></svg>
+                            </span>
+                            <span className="campaign-text">
+                                Ücretsiz İade
+                            </span>
+                        </div>
+                    </div>
+
 
                     <CommentsSection comments={product.comments || []} count={product.commentCount || 0} commentGridRef={commentGridRef} />
                 <OrderModal
